af0c66771eac596c92d0337c3e62abaa09de47f9
ouzhenxi <ouzhenxi@tuzhanai.com>
2020-05-11 17:54:26 +0800

    refactor: rename

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Nav from './components/Header/Nav';
 // import SubTabs from './components/SubTabs';
 import { Switch, Route, useParams, useRouteMatch } from 'react-router-dom';
-import Home from './Child/Home';
+import Home from './Child/Default';
 
 function Child() {
   let { menuType } = useParams();
