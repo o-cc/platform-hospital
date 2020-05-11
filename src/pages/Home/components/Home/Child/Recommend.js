adfa7ebf86d7cac7deb40fcc6f4cf65d5dceef52
ouzhenxi <ouzhenxi@tuzhanai.com>
2020-05-11 17:09:23 +0800

    fix: component lists without img

--- src/pages/Home/components/Home/Child/Recommend.js
+++ src/pages/Home/components/Home/Child/Recommend.js
@@ -1,36 +1,27 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: '100%',
-    backgroundColor: theme.palette.background.paper
+import Lists from './../../Lists';
+const recommendLists = [
+  {
+    text:
+      'In a list, you should ensure the Divider is rendered as an <li> to match the HTML5 specification.'
+  },
+  {
+    text:
+      'In a list, you should ensure the Divider is rendered as an <li> to match the HTML5 specification.'
+  },
+  {
+    text:
+      'In a list, you should ensure the Divider is rendered as an <li> to match the HTML5 specification.'
+  },
+  {
+    text:
+      'In a list, you should ensure the Divider is rendered as an <li> to match the HTML5 specification.'
+  },
+  {
+    text:
+      'In a list, you should ensure the Divider is rendered as an <li> to match the HTML5 specification.'
   }
-}));
-
+];
 export default function ListDividers() {
-  const classes = useStyles();
-
-  return (
-    <List component="nav" className={classes.root} aria-label="mailbox folders">
-      <ListItem button>
-        <ListItemText primary="In a list, you should ensure the Divider is rendered as an <li> to match the HTML5 specification." />
-      </ListItem>
-      <Divider />
-      <ListItem button divider>
-        <ListItemText primary="In a list, you should ensure the Divider is rendered as an <li> to match the HTML5 specification." />
-      </ListItem>
-      <ListItem button>
-        <ListItemText primary="In a list, you should ensure the Divider is rendered as an <li> to match the HTML5 specification." />
-      </ListItem>
-      <Divider light />
-      <ListItem button>
-        <ListItemText primary="In a list, you should ensure the Divider is rendered as an <li> to match the HTML5 specification." />
-      </ListItem>
-    </List>
-  );
+  return <Lists lists={recommendLists}></Lists>;
 }
---
+++ src/pages/Home/components/Lists/index.js
@@ -0,0 +1,35 @@
+import React, { Fragment } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+    '& .MuiListItem-root': {
+      paddingTop: 0,
+      paddingBottom: 0
+    }
+  }
+}));
+
+export default function ListDividers(props) {
+  const classes = useStyles();
+
+  const { lists } = props;
+  return (
+    <List component="nav" className={classes.root} aria-label="mailbox folders">
+      {lists.map((item, idx) => (
+        <Fragment key={idx}>
+          <ListItem button>
+            <ListItemText primary={item.text} />
+          </ListItem>
+          <Divider />
+        </Fragment>
+      ))}
+    </List>
+  );
+}
