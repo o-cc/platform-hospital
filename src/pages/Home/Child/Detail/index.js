b332480171487d667de1dbc81796ea797c13d732
ouzhenxi <ouzhenxi@tuzhanai.com>
2020-05-13 21:34:16 +0800

    feat: dangerousSetHTML

--- src/pages/Home/Child/Detail/index.js
+++ src/pages/Home/Child/Detail/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import { makeStyles, styled } from '@material-ui/core/styles';
 import { vw } from '@/utils';
-
+import { test_html } from 'configs/test_detail_html';
 const useStyles = makeStyles(theme => ({
   root: {
     marginTop: theme.spacing(8),
@@ -55,21 +55,7 @@ export default () => {
         </Grid>
 
         <Grid item xs={11}>
-          作为今年最重要的Windows
-          10更新，v2004还是很受用户关注的，当然它也是值得升级的。
-          现在，外媒就提前给出了Win10
-          v2004上改进的一些细节，其中系统对游戏性能上加强了不少，Windows显示驱动程序模型（WDDM）2.7，让多显示器设置提高刷新率，并且两个显示器之间的窗口移动似乎不会引起任何卡顿现象。
-          此外，WDDM更新还增加了对硬件加速GPU调度的支持，并允许GPU显卡正确管理其视频内存，这也让视频播放更加流畅。
-          Windows 10版本2004还增加了对DirectX Raytracing
-          1.1的支持，其中包括新技术，包括线上光线追踪和间接射线执行。在1.1版中，光线追踪执行是在运行时确定的，并且它们不是预先定义的。
-          如果你的电脑配备了HDD机械式硬盘，那么Windows 10
-          5月2020更新实际上感觉会更快。
-          对于SSD来说，你不太可能注意到任何明显的改进，因为你在它上加载的任何东西都已经可以更快地执行操作。固态硬盘的好处是，与HDD相比，它的等待时间已经大大减少。然而，在某些情况下，即使使用SSD固态硬盘，索引的工作仍然会影响到你的电脑性能，并占用CPU和磁盘资源。
-          当你在传输文件、删除文件，以及你的SSD或HDD磁盘正在积极使用时，Windows
-          10 2004版也会节流或完全停止任何索引活动。而Windows
-          Search节流索引活动的能力，将避免在SSD和HDD上出现这样的减速现象。
-          微软还在Windows
-          Search上改进逻辑工作，这样它可以智能地判断何时对你的文件进行索引，提高系统的整体性能。
+          <div dangerouslySetInnerHTML={{ __html: test_html }}></div>
         </Grid>
       </Grid>
     </>
