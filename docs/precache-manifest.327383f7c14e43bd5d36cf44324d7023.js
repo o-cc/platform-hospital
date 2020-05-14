43e4250bdd63f81b0f3583bcda8e998f9d0a31bf
ouzhenxi <ouzhenxi@tuzhanai.com>
2020-05-14 18:04:35 +0800

    test: docs

---
+++ docs/precache-manifest.327383f7c14e43bd5d36cf44324d7023.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5771244b844ae7092293d8bf2a352664",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9332c9013564f00f8f04",
+    "url": "/static/css/2.473716f9.chunk.css"
+  },
+  {
+    "revision": "03dec21db013acd06b13",
+    "url": "/static/css/main.ef4e0b16.chunk.css"
+  },
+  {
+    "revision": "9332c9013564f00f8f04",
+    "url": "/static/js/2.bea3baa6.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/static/js/2.bea3baa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03dec21db013acd06b13",
+    "url": "/static/js/main.975c46c0.chunk.js"
+  },
+  {
+    "revision": "3786a404376907893e66",
+    "url": "/static/js/runtime-main.8c210ae1.js"
+  },
+  {
+    "revision": "aa8f28f4b5efb8be788236570bc5548b",
+    "url": "/static/media/detail.aa8f28f4.jpeg"
+  }
+]);
