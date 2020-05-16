cf4851dd13e9974ef5ad04fce8c88f2900f77924
ouzhenxi <ouzhenxi@tuzhanai.com>
2020-05-16 22:54:49 +0800

    feat: add userInfo in detail page

--- src/pages/Home/Child/Detail/index.js
+++ src/pages/Home/Child/Detail/index.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
-import { Grid, Button, Divider, IconButton } from '@material-ui/core';
+import {
+  Grid,
+  Button,
+  Divider,
+  IconButton,
+  Link,
+  Avatar
+} from '@material-ui/core';
 import { makeStyles, styled } from '@material-ui/core/styles';
 import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
 import Comment from 'pages/Home/components/Comment';
@@ -10,7 +17,7 @@ import { withRouter } from 'react-router-dom';
 import InputComment from 'pages/Home/components/InputComment';
 const useStyles = makeStyles(theme => ({
   root: {
-    marginTop: theme.spacing(8),
+    marginTop: theme.spacing(6),
     fontFamily: 'Microsoft Yahei',
     fontSize: vw(30),
     color: '#000000',
@@ -18,11 +25,12 @@ const useStyles = makeStyles(theme => ({
     paddingBottom: theme.spacing(6)
   },
   date: {
-    alignSelf: 'flex-start',
+    alignSelf: 'center',
     fontSize: vw(20),
-    textAlign: 'left',
-    marginLeft: theme.spacing(2.5),
-    margin: theme.spacing(-1)
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '90%'
   },
   normalText: {
     color: '#333',
@@ -38,6 +46,25 @@ const useStyles = makeStyles(theme => ({
     alignSelf: 'flex-start',
     marginLeft: theme.spacing(3),
     width: '100%'
+  },
+  author: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '90%',
+    paddingTop: 0,
+    paddingBottom: 0
+  },
+  avatar: {
+    display: 'flex',
+    lineHeight: '20px',
+
+    '& .user': {
+      fontWeight: 'bold'
+    },
+    '& .date': {
+      color: '#888'
+    }
   }
 }));
 
@@ -45,8 +72,8 @@ const Img = styled(({ src, ...other }) => (
   <img src={src} alt="图片" {...other} />
 ))({
   width: '100%',
-  height: 'auto',
-  marginTop: vw(38)
+  height: 'auto'
+  // marginTop: vw(38)
 });
 
 function Detail(props) {
@@ -71,10 +98,31 @@ function Detail(props) {
         <Grid item xs={11}>
           微软改进Windows 10 v2004细节：整体游戏性能加强 支持光线追踪1.1
         </Grid>
-        <Grid item className={classes.date}>
-          2020年05月20日
+        <Grid
+          item
+          className={classes.date}
+          style={{ paddingTop: 0, paddingBottom: 0 }}
+          onClick={() => props.history.push('/user/user_id=1')}
+        >
+          <div className={classes.avatar}>
+            <Avatar alt="avatar" src={require('assets/imgs/test_avatar.jpg')} />
+            <Grid style={{ marginLeft: 8 }} container direction="column">
+              <span className="user">胖虎西西</span>
+              <Grid item className="date">
+                2020/05/20
+              </Grid>
+            </Grid>
+          </div>
+          <Link
+            href="#"
+            onClick={e => {
+              e.preventDefault();
+              e.stopPropagation();
+            }}
+          >
+            +关注
+          </Link>
         </Grid>
-
         <Grid item xs={11}>
           <Img src={require('assets/imgs/detail.jpeg')} />
         </Grid>
