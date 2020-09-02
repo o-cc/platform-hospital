ab83399c6a77d94408614a14c773967dea400116
ouzhenxi <ouzhenxi@tuzhanai.com>
2020-09-02 13:39:34 +0800

    fix: user page in pc

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -5,7 +5,8 @@ import {
   Paper,
   Avatar,
   styled,
-  Button
+  Button,
+  Hidden
 } from '@material-ui/core';
 import { vw, requestApi, keepPoint } from 'utils';
 import TopNav from 'pages/components/TopNav';
@@ -14,7 +15,8 @@ import Tabs from '@/pages/User/Tabs';
 import { useParams } from 'react-router-dom';
 import AppCont from 'container';
 import { defaultAvatar } from 'configs';
-
+import Nav from '../components/Header/Nav';
+import PCTemplate from 'pages/components/PCTemplate';
 const useStyles = makeStyles(theme => ({
   back: {
     alignSelf: 'flex-start',
@@ -23,38 +25,41 @@ const useStyles = makeStyles(theme => ({
   },
   info: {
     // width: '100%',
+
     paddingTop: theme.spacing(2),
     paddingBottom: theme.spacing(2),
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-around',
     fontFamily: 'Microsoft Yahei',
-    fontSize: vw(25),
+    fontSize: 14,
     color: '#000000',
-    lineHeight: vw(35)
+    lineHeight: 1.5,
+    '&>div:first-child': {
+      paddingLeft: 30,
+      maxWidth: 450
+    }
   },
   avatar: {
-    width: '90%',
-    height: 'auto'
+    width: 80,
+    height: 80
   },
-  introduce: {
-    paddingLeft: theme.spacing(1)
-  },
+  introduce: {},
   bold: {
     color: '#000',
     fontWeight: '600',
-    fontSize: vw(30)
+    fontSize: 16
   },
   small: {
     color: '#888',
-    fontSize: vw(25)
+    fontSize: 14
   }
 }));
 
 const Btn = styled(({ isAttention, ...other }) => (
   <Button size="small" {...other} variant="outlined"></Button>
 ))({
-  marginTop: vw(20),
+  marginTop: 16,
   color: props => (props.isAttention ? '#666' : '#fff'),
   width: '90%'
 });
@@ -94,92 +99,99 @@ export default props => {
   };
   return (
     <>
-      <TopNav title={userInfo.author} unDivider={false} />
-      <Paper variant="outlined" className={classes.info}>
-        <Grid container justify="center">
-          <Grid item xs={3}>
-            <Avatar
-              className={classes.avatar}
-              alt="avatar"
-              src={userInfo.avatar || defaultAvatar}
-            />
-          </Grid>
-          <Grid item xs={8}>
-            <FlexColumn>
-              <Grid container justify="space-around">
-                <Grid item>
-                  <FlexColumn>
-                    <span className={classes.bold}>
-                      {keepPoint(userInfo.news_count, 4)}
-                    </span>
-                    <span className={classes.small}>文章</span>
-                  </FlexColumn>
-                </Grid>
-                <Grid item>
-                  <FlexColumn>
-                    <span className={classes.bold}>
-                      {keepPoint(userInfo.followers_count, 4)}
-                    </span>
-                    <span className={classes.small}>关注</span>
-                  </FlexColumn>
-                </Grid>
-                <Grid item>
-                  <FlexColumn>
-                    <span className={classes.bold}>
-                      {keepPoint(userInfo.fans_count, 4)}
-                    </span>
-                    <span className={classes.small}>粉丝</span>
-                  </FlexColumn>
-                </Grid>
-                {userInfo.like_count && (
+      <Hidden smUp>
+        <TopNav title={userInfo.author} unDivider={false} />
+      </Hidden>
+      <Hidden xsDown>
+        <Nav />
+      </Hidden>
+      <PCTemplate>
+        <Paper variant="outlined" className={classes.info}>
+          <Grid container>
+            <Grid item>
+              <Avatar
+                className={classes.avatar}
+                alt="avatar"
+                src={userInfo.avatar || defaultAvatar}
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <FlexColumn>
+                <Grid container justify="space-around">
                   <Grid item>
                     <FlexColumn>
                       <span className={classes.bold}>
-                        {userInfo.like_count}
-                        {keepPoint(userInfo.fans_count, 4)}
+                        {keepPoint(userInfo.news_count, 4)}
                       </span>
-                      <span className={classes.small}>获赞</span>
+                      <span className={classes.small}>文章</span>
                     </FlexColumn>
                   </Grid>
-                )}
-              </Grid>
-            </FlexColumn>
-            {userInfo.has_follow && (
-              <Grid item style={{ textAlign: 'center' }}>
-                <Btn
-                  isAttention={userInfo.is_followed}
-                  style={{
-                    background: userInfo.is_followed ? '#fff' : '#f85a5b'
-                  }}
-                  onClick={attention}
-                >
-                  {userInfo.is_followed ? '已关注' : '关注'}
-                </Btn>
-              </Grid>
-            )}
+                  <Grid item>
+                    <FlexColumn>
+                      <span className={classes.bold}>
+                        {keepPoint(userInfo.followers_count, 4)}
+                      </span>
+                      <span className={classes.small}>关注</span>
+                    </FlexColumn>
+                  </Grid>
+                  <Grid item>
+                    <FlexColumn>
+                      <span className={classes.bold}>
+                        {keepPoint(userInfo.fans_count, 4)}
+                      </span>
+                      <span className={classes.small}>粉丝</span>
+                    </FlexColumn>
+                  </Grid>
+                  {userInfo.like_count && (
+                    <Grid item>
+                      <FlexColumn>
+                        <span className={classes.bold}>
+                          {userInfo.like_count}
+                          {keepPoint(userInfo.fans_count, 4)}
+                        </span>
+                        <span className={classes.small}>获赞</span>
+                      </FlexColumn>
+                    </Grid>
+                  )}
+                </Grid>
+              </FlexColumn>
+              {userInfo.has_follow && (
+                <Grid item style={{ textAlign: 'center' }}>
+                  <Btn
+                    isAttention={userInfo.is_followed}
+                    style={{
+                      background: userInfo.is_followed ? '#fff' : '#f85a5b'
+                    }}
+                    onClick={attention}
+                  >
+                    {userInfo.is_followed ? '已关注' : '关注'}
+                  </Btn>
+                </Grid>
+              )}
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid container justify="center">
-          <Grid item xs={11}>
-            <Grid
-              container
-              alignItems="flex-start"
-              style={{ paddingTop: vw(15) }}
-            >
-              <Grid item>
-                <AssignmentIcon
-                  size="small"
-                  style={{ color: '#555', marginLeft: '8px' }}
-                />
-              </Grid>
-              <Grid item xs={10}>
-                <div className={classes.introduce}>{userInfo.intro}</div>
+          <Grid container justify="center">
+            <Grid item xs={11}>
+              <Grid
+                container
+                alignItems="flex-start"
+                style={{ paddingTop: vw(15) }}
+              >
+                <Grid item>
+                  <AssignmentIcon
+                    size="small"
+                    style={{ color: '#555', marginLeft: '8px' }}
+                  />
+                </Grid>
+                <Grid item xs={10}>
+                  <div className={classes.introduce}>{userInfo.intro}</div>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
-        </Grid>
-      </Paper>
-      <Tabs userId={userId} setError={setError} />
+        </Paper>
+        <Tabs userId={userId} setError={setError} />
+      </PCTemplate>
     </>
   );
 };
